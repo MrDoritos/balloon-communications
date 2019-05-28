0c44740af8f8a3ba9e2006696f66a918a5ec5137
MrDoritos <ianmoore322@gmail.com>
2019-05-28 13:29:11 -0400

    Modified

--- arduino/client/client.ino
+++ arduino/client/client.ino
@@ -3,11 +3,11 @@
 
 serialClient sClient;
 
-timeSync tSync(&sClient);
-tempSensor tSense[3] = {tempSensor(&sClient, 1, &tSync),
-                        tempSensor(&sClient, 2, &tSync),
-                        tempSensor(&sClient, 3, &tSync)};
-uvSensor uvSense(&sClient, 1, &tSync);
+timeSync tSync(sClient);
+tempSensor tSense[3] = {tempSensor(sClient, 1, tSync),
+                        tempSensor(sClient, 2, tSync),
+                        tempSensor(sClient, 3, tSync)};
+uvSensor uvSense(sClient, 1, tSync);
 
 
 
@@ -16,9 +16,13 @@ void setup() {
   
   Serial.begin(9600);
 
-  tSync.send();
+  tSync.send(millis());
 }
 
 void loop() {
-  tSense[0].  
+  tSense[0].send(0);
+  tSense[1].send(1);
+  tSense[2].send(2);
+  uvSense.send(3);
+  delay(1000);
 }
